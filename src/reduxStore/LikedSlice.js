6613f0695b4957c05a6e7c9f22b57d979d7b6c88
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-22 01:26:02 +0500

    added  tosts when add and remove

--- src/reduxStore/LikedSlice.js
+++ src/reduxStore/LikedSlice.js
@@ -1,14 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
 const loadLikesFromStorage = () => {
   try {
     const storedLikes = localStorage.getItem("likedImages");
     return storedLikes ? JSON.parse(storedLikes) : [];
   } catch (error) {
-    console.error(error);
+    console.error("Error loading liked images from localStorage:", error);
     return [];
   }
 };
+
 const saveLikesToStorage = (likedImages) => {
   try {
     localStorage.setItem("likedImages", JSON.stringify(likedImages));
@@ -23,19 +25,39 @@ const likeSlice = createSlice({
     likedImages: loadLikesFromStorage(),
   },
   reducers: {
-    toggleLike: (state, action) => {
+    addLike: (state, action) => {
       const image = action.payload;
-      const index = state.likedImages.findIndex((img) => img.id === image.id);
-
-      if (index === -1) {
+      const exists = state.likedImages.some((img) => img.id === image.id);
+      if (!exists) {
         state.likedImages.push(image);
-      } else {
-        state.likedImages.splice(index, 1);
+        saveLikesToStorage(state.likedImages);
+        toast.success("✅ You Liked This Image", {
+          style: {
+            border: "1px solid #4CAF50",
+            padding: "10px",
+            color: "#4CAF50",
+            fontWeight: "bold",
+            background: "#f0fff0",
+          },
+        });
       }
+    },
+    removeLike: (state, action) => {
+      const imageId = action.payload;
+      state.likedImages = state.likedImages.filter((img) => img.id !== imageId);
       saveLikesToStorage(state.likedImages);
+      toast.success("removed!", {
+        style: {
+          border: "1px solid red",
+          padding: "10px",
+          color: "red",
+          fontWeight: "bold",
+          background: "rgb(227, 177, 177)",
+        },
+      });
     },
   },
 });
 
-export const { toggleLike } = likeSlice.actions;
+export const { addLike, removeLike } = likeSlice.actions;
 export default likeSlice.reducer;
