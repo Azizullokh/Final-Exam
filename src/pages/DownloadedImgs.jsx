2e13ea8e68ad9450267ae8421516fedd90974454
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-22 01:23:58 +0500

    added mansory style and fixed hover styles

--- src/pages/DownloadedImgs.jsx
+++ src/pages/DownloadedImgs.jsx
@@ -15,19 +15,19 @@ function DownloadedImgs() {
   const dispatch = useDispatch();
   const downloadedImages = useSelector((state) => state.download.images);
 
-  
+
   return (
-    <div>
+    <div className="px-4 max-w-6xl pt-25 flex mx-auto flex-col pb-10">
       <h1 className="text-xl font-bold mb-4">Downloads ({downloadedImages.length})</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="columns-2 sm:columns-3 md:columns-3 lg:columns-3 md:gap-4 gap-2 md:space-y-4 space-y-2">
         {downloadedImages.map((image) => (
-          <div key={image.id} className="relative">
+          <div key={image.id} className="relative break-inside-avoid overflow-hidden rounded-lg group">
             <img src={image.urls.small} alt={image.alt_description} className="w-full rounded-lg" />
             <div className="absolute top-3 right-3 flex gap-2">
-              <button onClick={() => saveToGallery(image.urls.full)} className="text-white bg-green-500 p-2 rounded">
+              <button onClick={() => saveToGallery(image.urls.full)} className="text-white border-2 border-transparent hover:border-white p-2 rounded transinion duration-300 cursor-pointer">
                 <FaSave />
               </button>
-              <button onClick={() => dispatch(removeImage(image.id))} className="text-white bg-red-500 p-2 rounded">
+              <button onClick={() => dispatch(removeImage(image.id))} className="text-white border-2 border-transparent hover:border-white p-2 rounded transinion duration-300 cursor-pointer">
                 <FaTrash />
               </button>
             </div>
