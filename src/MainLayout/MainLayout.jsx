4521f41779e29f7815d2ccb135d4a27d80ae2d7d
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-22 01:22:13 +0500

    fixed darkmode issue

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -5,7 +5,7 @@ const MainLayout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar></Navbar>
-      <div className="flex-grow w-full">{children}</div>
+      <div className="flex-grow w-full bg-gray-100 dark:bg-gray-900">{children}</div>
     </div>
   );
 };
