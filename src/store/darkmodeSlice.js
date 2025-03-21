80934e859a966e287488520295708c93f5bc90db
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-21 16:33:39 +0500

    removed system theme

--- src/store/darkmodeSlice.js
+++ src/store/darkmodeSlice.js
@@ -1,17 +1,10 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: "system",
+  theme: "light",
   setTheme: (theme) => {
     set({ theme });
-    if (theme === "system") {
-      const systemTheme = window.matchMedia("(prefers-color-scheme: black)").matches ? "dark" : "light";
-      document.documentElement.classList.remove("light", "dark");
-      document.documentElement.classList.add(systemTheme);
-    } else {
-      document.documentElement.classList.remove("light", "dark");
-      document.documentElement.classList.add(theme);
-    }
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
   },
 }));
-3
