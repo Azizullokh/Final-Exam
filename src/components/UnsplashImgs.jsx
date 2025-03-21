8ddb15425f28ccc4c098e6f7f306ac4a4e982f59
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-21 20:15:10 +0500

    added hover effect for mobile

--- src/components/UnsplashImgs.jsx
+++ src/components/UnsplashImgs.jsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import debounce from "lodash.debounce";
-import { useDispatch, useSelector } from "react-redux"; 
+import { useDispatch, useSelector } from "react-redux";
 import { FiDownload } from "react-icons/fi";
-import { FaRegHeart , FaHeart } from "react-icons/fa6";
+import { FaRegHeart, FaHeart } from "react-icons/fa6";
 import { GoDownload } from "react-icons/go";
 import { FaSearch } from "react-icons/fa";
 import { toggleLike } from "../reduxStore/LikedSlice";
+import { addImage } from "../reduxStore/downloadSlice";
 
 const UnsplashImgs = () => {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [hoveredImage, setHoveredImage] = useState(null);
   const dispatch = useDispatch();
   const likedImages = useSelector((state) => state.likes.likedImages);
+  const downloadedImages = useSelector((state) => state.download.images);
 
   const fetchImages = async (searchQuery, pageNum) => {
     const accessKey = "BdNtKrF0I9Pg2gbNGzUBKQPnuT5r2Rr4F_2u8EbtI-0";
@@ -60,47 +63,61 @@ const UnsplashImgs = () => {
           />
           <FaSearch className="absolute right-4 top-4" />
         </div>
-      </div><div className="flex mx-auto items-center justify-center">
-            {loading && <span className="loading loading-ring loading-xl"></span>}
-        </div>
+      </div>
+      <div className="flex mx-auto items-center justify-center">
+        {loading && <span className="loading loading-ring loading-xl"></span>}
+      </div>
       <div className="columns-2 sm:columns-3 md:columns-3 lg:columns-3 md:gap-4 gap-2 md:space-y-4 space-y-2">
-        
         {images.map((image) => {
           const isLiked = likedImages.some((img) => img.id === image.id);
-
+          const isDownloaded = downloadedImages.some(
+            (img) => img.id === image.id
+          );
           return (
             <div
               key={image.id}
               className="relative break-inside-avoid overflow-hidden rounded-lg group"
+              onMouseEnter={() => setHoveredImage(image.id)}
+              onMouseLeave={() => setHoveredImage(null)}
+              onTouchStart={() => setHoveredImage(hoveredImage === image.id ? null : image.id)}
             >
               <img
                 src={image.urls.small}
                 alt={image.alt_description}
                 className="w-full rounded-lg transition-transform duration-300 group-hover:scale-105"
               />
-              <div className="absolute inset-0 flex flex-col justify-end p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black/60">
+              <div
+                className={`absolute inset-0 flex flex-col justify-end p-4 transition-opacity duration-300 bg-black/60 ${
+                  hoveredImage === image.id ? "opacity-100" : "opacity-0"
+                }`}
+              >
                 <div className="absolute bottom-1 left-2 md:bottom-2 md:left-4 flex items-center gap-2 mb-2">
                   <img
                     src={image.user.profile_image.small}
                     alt={image.user.name}
                     className="md:w-8 md:h-8 w-5 h-5 rounded-full"
                   />
-                  <span className="text-white md:text-2xl text-[10px]">{image.user.name}</span>
+                  <span className="text-white md:text-2xl text-[10px]">
+                    {image.user.name}
+                  </span>
                 </div>
                 <div className="absolute top-3 right-3 flex gap-2 justify-between">
                   <button
-                    className={`md:text-2xl text-xl ${isLiked ? "text-red-500" : "text-white"}`}
+                    className={`md:text-2xl text-xl ${
+                      isLiked ? "text-red-500" : "text-white"
+                    }`}
                     onClick={() => dispatch(toggleLike(image))}
                   >
                     {isLiked ? <FaHeart /> : <FaRegHeart />}
                   </button>
-                  <a
-                    href={image.links.download}
-                    target="_blank"
-                    className="text-white md:text-2xl text-xl"
+                  <button
+                    className={`text-white md:text-2xl text-xl ${
+                      isDownloaded ? "text-blue-500" : ""
+                    }`}
+                    onClick={() => dispatch(addImage(image))}
                   >
                     <FiDownload />
-                  </a>
+                  </button>
                 </div>
               </div>
             </div>
