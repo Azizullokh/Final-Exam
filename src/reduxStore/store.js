2989e78bc8608e5825456b40912c9ec1131e63f9
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-21 20:19:14 +0500

    added download reducer

--- src/reduxStore/store.js
+++ src/reduxStore/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import likeReducer from "./LikedSlice";
+import downloadReducer from "./downloadSlice"
 
 const store = configureStore({
   reducer: {
     likes: likeReducer,
+    download: downloadReducer,
   },
 });
 
