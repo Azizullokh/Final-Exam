980305f1613c38d950f2b5e425a79a938078bed6
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-22 15:37:12 +0500

    new function save mode to local storage

--- src/store/darkmodeSlice.js
+++ src/store/darkmodeSlice.js
@@ -1,10 +1,21 @@
 import { create } from "zustand";
+import { useEffect } from "react";
 
 export const useThemeStore = create((set) => ({
-  theme: "light",
+  theme: localStorage.getItem("theme") || "light",
   setTheme: (theme) => {
     set({ theme });
+    localStorage.setItem("theme", theme);
     document.documentElement.classList.remove("light", "dark");
     document.documentElement.classList.add(theme);
   },
 }));
+
+export const useApplyTheme = () => {
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+};
