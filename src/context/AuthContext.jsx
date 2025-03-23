c39c9904a2e8cddf0416a2159fd08ca50c10012a
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-23 10:31:08 +0500

    added toasts

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { signInWithEmailAndPassword, sendPasswordResetEmail, onAuthStateChanged, signOut, signInWithPopup, updateProfile } from "firebase/auth";
 import { auth, googleProvider } from "../firebase";
+import { toast } from "react-hot-toast";
 
 
 const AuthContext = createContext();
@@ -18,7 +19,7 @@ export const AuthProvider = ({ children }) => {
           uid: currentUser.uid,
           email: currentUser.email,
           displayName: currentUser.displayName,
-          photoURL: currentUser.photoURL || "",
+          photoURL: currentUser.photoURL,
         });
       } else {
         setUser(null);
@@ -33,45 +34,57 @@ export const AuthProvider = ({ children }) => {
   const loginUser = async (email, password) => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      console.log("Tizimga muvaffaqiyatli kirdingiz!");
       return true;
     } catch (error) {
-      console.error("Login Error:", error.message);
-      alert("Login xatosi: " + error.message);
+      console.log(error);
       return false;
     }
   };
 
-
-
-
   const resetPassword = async (email) => {
     try {
       await sendPasswordResetEmail(auth, email);
-      console.log("Password reset email sent!");
-      alert("Parolni tiklash uchun email yuborildi!");
+      toast.success("Password reset email sent!", {
+        style: {
+          border: "1px solid #4CAF50",
+          padding: "10px",
+          color: "#4CAF50",
+          fontWeight: "bold",
+          background: "#f0fff0",
+        },
+      });
     } catch (error) {
-      console.error("Reset Password Error:", error.message);
-      alert("Parolni tiklashda xato yuz berdi: " + error.message);
+      toast.error("Reset Password Error" ,error, {
+        style: {
+          border: "1px solid rgb(227, 61, 61)",
+          padding: "10px",
+          color: "red",
+          fontWeight: "bold",
+          background: "rgb(248, 196, 196)",
+        },
+      });
     }
   };
-
-
 
 
   const loginWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
       setUser(result.user);
-      setProfileImage(result.user.photoURL || "default-avatar.png");
-      localStorage.setItem("profileImage", result.user.photoURL || "default-avatar.png");
+      setProfileImage(result.user.photoURL);
+      localStorage.setItem("profileImage", result.user.photoURL);
     } catch (error) {
-      alert("Google bilan kirishda xatolik: " + error.message);
+      toast.success(error, {
+        style: {
+          border: "1px solid rgb(227, 61, 61)",
+          padding: "10px",
+          color: "red",
+          fontWeight: "bold",
+          background: "rgb(248, 196, 196)",
+        },
+      });
     }
   };
-
-
-
 
   const updateUserProfile = async (newDisplayName, newPhotoURL) => {
     try {
@@ -91,12 +104,10 @@ export const AuthProvider = ({ children }) => {
         localStorage.setItem("profileImage", newPhotoURL);
       }
     } catch (error) {
-      console.error("Profilni yangilashda xato:", error.message);
+      console.log(error);
     }
   };
 
-
-  
 
   const logoutUser = async () => {
     await signOut(auth);
