7c30642478a82bc4706932d0aba8ea33fb32bca5
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-23 23:34:57 +0500

    I moved that function from authContext

--- src/components/SignInwithGoogle.jsx
+++ src/components/SignInwithGoogle.jsx
@@ -1,13 +1,40 @@
 import { FcGoogle } from "react-icons/fc";
-import { useAuth } from '../context/AuthContext';
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { auth, googleProvider } from "../firebase";
+import { useState } from "react";
 
 const GoogleLogin = () => {
-  const { loginWithGoogle } = useAuth();
+  const navigate = useNavigate()
+  const [user , setUser] = useState(null)
+  const [profileImage, setProfileImage] = useState(
+    localStorage.getItem("profileImage")
+  );
+  const loginWithGoogle = async () => {
+      try {
+        const result = await signInWithPopup(auth, googleProvider);
+        setUser(result.user);
+        setProfileImage(result.user.photoURL);
+        localStorage.setItem("profileImage", result.user.photoURL);
+        navigate("/")
+      } catch (error) {
+        toast.success(error, {
+          style: {
+            border: "1px solid rgb(227, 61, 61)",
+            padding: "10px",
+            color: "red",
+            fontWeight: "bold",
+            background: "rgb(248, 196, 196)",
+          },
+        });
+      }
+    };
 
   return (
-    <div className="flex items-center justify-center gap-3 bg-black p-3 rounded-lg">
+    <div onClick={loginWithGoogle} className="flex items-center justify-center gap-3 bg-black p-3 rounded-lg cursor-pointer">
       <FcGoogle />
-      <button onClick={loginWithGoogle}>Continue with Google</button>
+      <button className="cursor-pointer">Continue with Google</button>
     </div>
   );
 };
